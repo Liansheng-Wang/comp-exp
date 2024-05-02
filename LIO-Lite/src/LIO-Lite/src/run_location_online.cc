592a2926249f93f671eac8f859390358c90096c5
Liansheng-Wang <valiant_dasheng@163.com>
2024-05-02 14:12:56 +0800

    Using lio_lite instead of faster_lio

---
+++ LIO-Lite/src/LIO-Lite/src/run_location_online.cc
@@ -0,0 +1,48 @@
+
+#include <unistd.h>
+#include <csignal>
+
+#include "custom_logs.hpp"
+#include "laser_mapping.h"
+
+DEFINE_bool(need_logs, true, "save logs for check");
+void SigHandle(int sig) {
+    lio_lite::options::FLAG_EXIT = true;
+    ROS_WARN("catch sig %d", sig);
+}
+
+int main(int argc, char **argv) {
+    google::InitGoogleLogging(argv[0]);
+    init_log();
+    if(FLAGS_need_logs){
+        save_log("local");
+    }
+    ros::init(argc, argv, "lio_lite");
+    ros::NodeHandle nh;
+    
+    LOG(INFO) << "\033[1;32m run_location_online \033[0m";
+
+    auto laser_mapping = std::make_shared<lio_lite::LaserMapping>();
+    laser_mapping->InitROS(nh);
+
+    signal(SIGINT, SigHandle);
+    ros::Rate rate(5000);
+    
+    laser_mapping->Load_map();
+
+    while (ros::ok()) {
+        if (lio_lite::options::FLAG_EXIT) {
+            break;
+        }
+        ros::spinOnce();
+        laser_mapping->Run_location();
+        rate.sleep();
+    }
+
+    LOG(INFO) << "finishing!";
+    laser_mapping->Finish();
+
+    lio_lite::Timer::PrintAll();
+
+    return 0;
+}
